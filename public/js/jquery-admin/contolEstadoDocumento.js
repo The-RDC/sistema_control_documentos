6e1971469eeedb940c74a336f261bdb4548c3067
Helio Mejia M <mm.helio009@gmail.com>
2023-04-12 18:17:15 -0400

    Se cambio el nombre de las opciones de la barra de navegacion  a lo estandar, asi mismo se hizo el jqyeru para marcar el estado finalizado de los documentos y desabilitar los botonos

---
+++ public/js/jquery-admin/contolEstadoDocumento.js
@@ -0,0 +1,25 @@
+/**
+ * Funcion que captura todos los tr de los registros de documentos, y marcarlos
+ * con rojo (toda la linea). Asi tambien desabilita el boton que finaliza el estado de un documento
+ */
+
+function marcarEstadoDocumentoFinalizado()
+{
+   let estadoActualDocumento =$('#idEstadoDocumento').text();
+   let estadoParaComparar="1";
+   $("#controlDeEstadoDocumentoParaMarcarlo tr").each( function (indexInArray, valueOfElement) 
+   { 
+     if($(this).find("#idEstadoDocumento").text() == estadoParaComparar)
+     {
+        $(this).css("background","#e74c3c");
+        $(this).css("color","black");
+        $(this).find("#btnEditarDocumento").removeAttr("href");
+        $(this).find("#btnEstadoFinalizar").attr("disabled",true);
+        $(this).find("#btnElimiarDocumento").attr("disabled",true);
+     }
+   });
+}
+//setInterval(marcarEstadoDocumentoFinalizado,5000);
+setTimeout(marcarEstadoDocumentoFinalizado,1000);
+
+

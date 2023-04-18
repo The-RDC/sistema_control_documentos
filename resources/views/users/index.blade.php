8c947e74e1068af6bd40ab096c7e805d1e3422ff
Helio Mejia M <mm.helio009@gmail.com>
2023-04-18 15:45:33 -0400

    se agrego la funcionalidad de confirmacion de eliminacion de registro en la vista users create e index

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -47,9 +47,15 @@
                 <td>
                     <a class="btn btn-info" href="{{ route('users.show',$user->id) }}">Ver</a>
                     <a class="btn btn-primary" href="{{ route('users.edit',$user->id) }}">Editar</a>
-                    {!! Form::open(['method' => 'DELETE','route' => ['users.destroy', $user->id],'style'=>'display:inline']) !!}
-                    {!! Form::submit('Borrar', ['class' => 'btn btn-danger']) !!}
+                    {!! Form::open(['method' => 'DELETE','route' => ['users.destroy', $user->id],'style'=>'display:inline','id'=>$user->id]) !!}
+                    {!! Form::submit('Borrar', ['class' => 'btn btn-danger','data-descripcion' => 'BorrarRegistroTablas']) !!}
                     {!! Form::close() !!}
+                    <form action="{{route('users.destroy',$user->id)}}" method="post" id="{{$user->id}}">
+                        @csrf @method('DELETE')
+                        <button class="btn btn-md btn-danger" data-toggle="tooltip" data-placement="top" title="Eliminar" data-descripcion="BorrarRegistroTablas">
+                            Borrar
+                        </button>
+                    </form>
                 </td>
             </tr>
         @endforeach

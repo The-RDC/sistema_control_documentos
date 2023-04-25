d55b3a74b8849797010def3039ed7a2b16a990ca
Helio Mejia M <mm.helio009@gmail.com>
2023-04-25 10:17:12 -0400

    vista empleado-sucursales-empresa-regional

--- resources/views/empleado/create.blade.php
+++ resources/views/empleado/create.blade.php
@@ -16,45 +16,30 @@
                             </form>
                         </div>
                     </div>
+
                     <div class="col-lg-4" style="margin-top: 100px;">
-                        <div class="text-center">
-                            <h1 class="h4 text-gray-900 mb-4">Seleccione la Empresa y Sucursal del Empleado</h1>
-                        </div>
-                        
+                            <div class="text-center">
+                                <h1 class="h4 text-gray-900 mb-4">Seleccione la Empresa y Sucursal del Empleado</h1>
+                            </div>
                             <div class="accordion" id="accordionEmpresa">
-                                <div class="card">
-                                    <div class="card-header" id="headingOne">
-                                        <h2 class="mb-0">
-                                        <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                                            Empresa:
-                                        </button>
-                                        </h2>
-                                    </div>
-                                
-                                    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionEmpresa">
-                                        <div class="card-body">
-                                        Some placeholder content for the first accordion panel. This panel is shown by default, thanks to the <code>.show</code> class.
+                                @for ($i = 0; $i < 2; $i++)
+                                    <div class="card">
+                                        <div class="card-header" id="headingOne{{$i+1}}">
+                                            <h2 class="mb-0">
+                                            <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseOne{{$i+1}}" aria-expanded="@php echo $i==0?true:false @endphp" aria-controls="collapseOne">
+                                                Empresa: {{$i+1}}
+                                            </button>
+                                            </h2>
+                                        </div>
+                                    
+                                        <div id="collapseOne{{$i+1}}" class="collapse @php echo $i==0?"show":"" @endphp" aria-labelledby="headingOne{{$i+1}}" data-parent="#accordionEmpresa">
+                                            <div class="card-body">
+                                                Some placeholder content for the first accordion panel. This panel is shown by default, thanks to the <code>.show</code> class.
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                                @for ($i = 0; $i < 5; $i++)
-                                <div class="card">
-                                    <div class="card-header" id="headingTwo">
-                                      <h2 class="mb-0">
-                                        <button class="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo{{$i+2}}" aria-expanded="false" aria-controls="collapseTwo{{$i+2}}">
-                                          Collapsible Group Item #2
-                                        </button>
-                                      </h2>
-                                    </div>
-                                    <div id="collapseTwo{{$i+2}}" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
-                                      <div class="card-body">
-                                        Some placeholder content for the second accordion panel. This panel is hidden by default.
-                                      </div>
-                                    </div>
-                                </div>
                                 @endfor
                             </div>
-                        
                     </div>
                 </div>
             </div>

90b45efb159252d51adc63d4cc00b55e1b23b5bf
Helio Mejia M <mm.helio009@gmail.com>
2023-05-10 11:06:50 -0400

    se borro la columna observacion de la tabla registro_documentos, asi mismo se agrego una nueva table de observacion (migrate y model) para manejar la observacion por estado del documento

---
+++ app/Models/observacion.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class observacion extends Model
+{
+    use HasFactory;
+    protected $table="observacion";
+    protected $guarded=[];
+    
+}
